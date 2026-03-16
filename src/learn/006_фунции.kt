2dae2e69c48fb0988b99e900a7b92cfd49044194
eurovomit <eurovomit@gmail.com>
2026-03-16 14:26:45 +0200

    add fun

--- src/learn/006_фунции.kt
+++ src/learn/006_фунции.kt
@@ -1,22 +1,5 @@
 package learn
 
-fun main() {
-    println(testFun0(6, 2))
-
-//    вызов с именованными аргументами по умолчании
-    println(testFun1())
-    println(testFun1(5))
-    println(testFun1(two = 9))
-
-    println(testFun2())
-    println(testFun3())
-
-    println(testFun4())
-    println(testFun5())
-
-    println(testFun6(5))
-    println(testFun6(5, 6))
-}
 
 private fun testFun0(one: Int, two: Int): Int {
     val result = one * two
@@ -34,10 +17,10 @@ private fun testFun2(one: Int = 2, two: Int = 4): Int = one * two
 private fun testFun3(one: Int = 2, two: Int = 4) = one * two
 
 //если функция ничего не возвращает, она имеет тип Unit, этот тип сделан для работы с обобщениями
-private fun testFun4() {println("test")}
-private fun testFun5(): Unit {println("test")}
+private fun testFun4() = println("test")
+private fun testFun5(): Unit = println("test")
 
 //прегрузка функций, одно и то же имя, но разная реализация из-за разных аргументов
-private fun testFun6(one: Int): Int {return one}
-private fun testFun6(one: Int, two: Int) {println("7")}
+private fun testFun6(one: Int): Int = one
+private fun testFun6(one: Int, two: Int) = println("первый аргумент $one, сумма ${one + two}")
 
