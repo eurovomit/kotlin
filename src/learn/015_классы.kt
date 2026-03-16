2422d5f27873eeb2aad796fcd38c2073cb60c1f8
eurovomit <eurovomit@gmail.com>
2026-03-16 16:12:49 +0200

    add class

--- src/learn/015_классы.kt
+++ src/learn/015_классы.kt
@@ -1,11 +1,32 @@
 package learn
 
+//пустой класс
+class Circle0
+
+
+//класс с конструктором
+class Circle1(val radius: Double)
+
+
+//класс с конструктором, полем и методом
+class Circle2(val radius: Double) {
+    val pi: Double = 3.14159265358979
+    fun circumference(): Double = radius * pi * 2
+}
+
+
+//объект, у которого возможно создать только экземпляр
+object Circle3 {
+    val pi: Double = 3.14159265358979
+}
+
+
 class Player {
     val name = "gregor"
         get() = field.replaceFirstChar { it.uppercase() }   //    вызывается автоматически при обращении к полю
     var age = 39
         get() = field - 1       //    вызывается автоматически при обращении к полю, видимость такая же как у поля, менять ее смысла нет
-        public set(value) {field = value + 1}   //    вызывается автоматически при обращении к полю, видимость методов не может быть шире видимости поля
+        public set(value) {field = value + 2}   //    вызывается автоматически при обращении к полю, видимость методов не может быть шире видимости поля
     var rank = "assassin"
         private set                             //        сокращенная запись, если просто нельзя изменить вне класса
     val fullData: String                //    вычисляемое свойство
